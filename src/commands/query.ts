8ee10aa8bbdbc1440a80a785ce3901e4956c3428
Archie Hilton (thytom) <archie.hilton1@gmail.com>
2021-09-09 21:26:58 +0100

    feat: Add query command
    This command allows a user to make a query on the Students table from
    within Discord.

---
+++ src/commands/query.ts
@@ -0,0 +1,84 @@
+import * as Discord from 'discord.js'
+import type {Command} from '../app'
+import {setupDatabase} from '../util/RegisterDatabase'
+import {DatabaseAccessor} from '../util/DatabaseAccessor'
+
+import config from '../../config/config.json'
+import {interactionReply} from '../util/InteractionUtils'
+
+const command : Command = {
+	disable:false,
+	name:'query',
+	description:'Look up a student in the database.',
+	options: [
+		{
+			name:'id',
+			description:'Look up a student based on discord account.',
+			type:'SUB_COMMAND',
+			options: [
+				{
+					name: 'user',
+					description:'The user to look up',
+					type: 'USER',
+					required:true
+				}
+			]
+		},
+		{
+			name:'name',
+			description:'Look up students that match the full or partial name given.',
+			type:'SUB_COMMAND',
+			options: [
+				{
+					name:'name',
+					description:'The name to query',
+					type:'STRING',
+					required:true
+				}
+			]
+
+		}
+	],
+	initHooks: [
+		setupDatabase,
+	],
+	defaultPermission:false,
+	permissions:['Mentor', 'admin'],
+	lockToChannels:['mentor-room'],
+	execute: (interaction, args) => {
+		const sc = args.getSubcommand();
+		var sqlQuery = "";
+		switch(sc) {
+			case 'id': 
+				const user = args.getUser('user');
+				sqlQuery = `discordID = '${user.id}'`;
+				break;
+			case 'name': 
+				const name = args.getString('name');
+				sqlQuery = `fullName LIKE '${name}'`;
+				break;
+		}
+
+		const fullQuery = `SELECT * FROM Students where ${sqlQuery}`;
+
+		new DatabaseAccessor(config.databaseLocation).querySQL(fullQuery)
+		.then(result => {
+			const stringList = result.map(r => {
+				const present = !(r.discordID == null);
+				return `name: ${r.fullName}\npresent: ${present}`;
+			})
+			if(stringList.length > 0) {
+				interactionReply(interaction, stringList.join('\n'), false);
+			} else {
+				interactionReply(interaction, "No results found.", false);
+			}
+		}).catch(err => {
+			interactionReply(interaction, "Sorry, something went wrong.", true);
+			console.log(err);
+		})
+	}
+}
+
+module.exports = {
+	command
+};
