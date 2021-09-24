5b10eadf5349cfe7405d79ff1950cb6a80f83e51
Archie Hilton (thytom) <archie.hilton1@gmail.com>
2021-09-24 12:27:51 +0100

    feat: Add ConfigurationManager class, public/private access
    Reinstates the ConfigurationManager class alongside public/private
    access for the fetch command.
    
    If at any point during the JSON resolution a "private" boolean with the
    value "true" is encountered in the subtree, the value to be searched for
    is considered private.

--- src/util/ConfigurationManager.ts
+++ src/util/ConfigurationManager.ts
@@ -1,52 +1,95 @@
-
-import config from '../../config/config.json'
 import { DatabaseAccessor } from './DatabaseAccessor'
 
-export async function fetch(configurableOption : string):Promise<string> {
-	const defaultValue = jsonResolve(config, configurableOption);
+import fs from 'fs'
 
-	var dbQueryResult = [];
+import {databaseLocation} from '../../config/private.json'
 
-	try {
-		dbQueryResult = await new DatabaseAccessor(config.databaseLocation)
-		.querySQL(`SELECT * FROM Config WHERE name='${configurableOption}'`);
-	} catch (err) {
-		console.log("Error fetching from database: " + err);
+export class ConfigurationManager {
+
+	private static configurationDirectory = '../../config/';
+
+	private files:any;
+
+	public constructor() {
+		// Load all configuration files
+		this.files = this.loadFiles();
 	}
 
-	if(dbQueryResult.length == 0) {
-		return defaultValue;
-	} else {
-		return dbQueryResult[0].value;
+	private async loadFiles():Promise<any>{
+		var files = {};
+		const filenames = fs.readdirSync(ConfigurationManager.configurationDirectory)
+		.filter(file => !file.endsWith('template.json')) // Filter out default files
+		.map((f:String) => f.replace('.json', ''))
+		for(const file of filenames) {
+			files[file] = require(ConfigurationManager.configurationDirectory + file);
+		}
+		return files;
 	}
-}
 
-export async function set(configurableOption : string, value:string):Promise<void> {
-	const sqlupdate = `
-	UPDATE Config SET value='${value}' WHERE name='${configurableOption}';`;
-	const sqlnew = `INSERT INTO Config (name, value) VALUES ('${configurableOption}', '${value}');`;
-	const changes = await new DatabaseAccessor(config.databaseLocation)
-	.runSQL(sqlupdate);
+	public async fetch(configurableOption:string, allowPrivate:boolean = false):Promise<string> {
+		const defaultValue = jsonResolve(await this.files, configurableOption, allowPrivate);
 
-	if(changes == 0)
-		await new DatabaseAccessor(config.databaseLocation)
-		.execSQL(sqlnew);
-}
+		var dbQueryResult = [];
 
-async function jsonResolve(jsonObj:any, str:string) : Promise<string> {
-	const parts = str.split('.');
-	var tmp:any = jsonObj
+		try {
+			dbQueryResult = await new DatabaseAccessor('../../' + databaseLocation)
+			.querySQL(`SELECT * FROM Config WHERE name='${configurableOption}'`);
+		} catch (err) {
+			console.log("Error fetching from database: " + err);
+		}
 
-	for(const part of parts) {
-		tmp = tmp[part];
-		if (tmp === undefined) {
-			throw new Error("Option does not exist.");
+		if(dbQueryResult.length == 0) {
+			return defaultValue;
+		} else {
+			return dbQueryResult[0].value;
 		}
 	}
 
-	if(typeof tmp != "string"){
+	// TODO: 
+	// 	- Set should only work on public values
+	//  - Confirm a value actually exists before trying to set it
+	public async set(configurableOption : string, value:string):Promise<void> {
+		const sqlupdate = `
+		UPDATE Config SET value='${value}' WHERE name='${configurableOption}';`;
+		const sqlnew = `INSERT INTO Config (name, value) VALUES ('${configurableOption}', '${value}');`;
+		const changes = await new DatabaseAccessor(databaseLocation)
+		.runSQL(sqlupdate);
+
+		if(changes == 0)
+			await new DatabaseAccessor(databaseLocation)
+			.execSQL(sqlnew);
+	}
+
+}
+
+async function jsonResolve(jsonObj:any, str:string, allowPrivate:boolean = false) : Promise<string> {
+	const parts = str.split('.');
+	var tmp:any = jsonObj;
+
+	var isPrivate:boolean = false;
+
+	for(const part of parts) {
+		if(!allowPrivate) {
+			if(tmp[part]?.private === true) {
+				isPrivate = true;
+			}
+		}
+		if (tmp === undefined) {
+			throw new Error("Option does not exist.");
+		}
+		tmp = tmp[part];
+	}
+
+	if(typeof tmp != "string"
+	  && typeof tmp != "boolean"
+	  && typeof tmp != "number"){
 		throw new Error("Option does not exist.");
 	}
 
-	return tmp;
+	if(!allowPrivate
+	  && isPrivate) {
+		throw new Error("Public request for private option.");
+	}
+
+	return tmp.toString();
 }
